f30b4614c30df3bdea6147f6599f94bb0ff287ad
hiro-gpt <hiro.siervo08@icloud.com>
2025-12-27 04:25:47 -0800

    Update conditional-statements.cpp

--- conditional-statements.cpp
+++ conditional-statements.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     string n_temp;
@@ -8,35 +7,11 @@ int main()
 
     int n = stoi(ltrim(rtrim(n_temp)));
 
-    if(n==1){
-        cout << "one";
-    }
-    else if(n==2){
-        cout << "two";
-    }
-    else if(n==3){
-        cout << "three";
-    }
-    else if(n==4){
-        cout << "four";
-    }
-    else if(n==5){
-        cout << "five";
-    }
-    else if(n==6){
-        cout << "six";
-    }
-    else if(n==7){
-        cout << "seven";
-    }
-    else if(n==8){
-        cout << "eight";
-    }
-    else if(n==9){
-        cout << "nine";
+    vector<string> numWords={"one", "two", "three", "four", "five", "six", "sevem", "eight", "nine"}
+    if (n>=1 && n<=9) {
+        cout << words[n-1];
     }
     else {
         cout << "Greater than 9";
     }
-    return 0;
 }
